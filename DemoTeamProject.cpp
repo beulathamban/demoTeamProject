c70e0fecd42d4ffeb467d7e53ea72992417eb32e
Beula Samuel <beula.samuel@yoobeecolleges.com>
2022-05-17 14:33:12 +1200

    callFunction2

--- DemoTeamProject.cpp
+++ DemoTeamProject.cpp
@@ -7,12 +7,17 @@ void callFunction1() {
     std::cout << "\nfrom callFunction1";
 }
 
+
+void callFunction2() {
+    std::cout << "\nfrom callFunction1";
+}
+
 int main()
 {
     std::cout << "Hello World!\n"; //First iteration Beula
     std::cout << "Hello Beula\n";//First iteration Samuel
     std::cout << "Hello Samuel\n"; //Second iteration Beula
     std::cout << "Let\'s get started";//Second iteration Samuel
-    callFunction1();
+    callFunction2();
 
 }
